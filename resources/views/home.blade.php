4ce4524c63bb08c4362470787d05185fcb84c219
Arsyadam <arsyadamid.work@gmail.com>
2021-09-25 13:02:20 +0700

    ui halaman home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,60 +3,58 @@
 
 @section('body')
 <style>
+    .grayscale {filter: grayscale(75%);}
+    .form-label {
+        color:#889E81;
+        font-family: 'Comfortaa', sans-serif;
+        font-weight: bold;
+    }
+    div.relative {
+      position: relative;
+      width: 100%;
+      height: 200px;
+     
+    } 
     
-    .centered {
-        position: absolute;
-        top: 32%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        background-color:#E5E4CC;
-    }
-    .center {
-        position: absolute;
-        top: 20%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        color: white;
-    }
-    .btn-default {
-        margin-top: 33px;
-        background-color: white;
-        padding: 10px;
-
-    }
-    .large {
-        width: 15rem;
-        height: 3em;
-    }
-</style>
-<div class="center">
-    <h1 style="font-size: 5rem">Cari harga transportasi online yang sesuai</h1>
-</div>
-<div class=" centered px-5 py-3" >
-    <div class="container" style="">
     
-    <form> 
-        <div class="d-flex">
-            <div class=" p-0 m-0">
-                <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control large rounded-start" id="exampleInputPassword1">
+    </style>
+    
+    <div class="card bg-dark text-white " style="border:none;">
+        <img src="img/home.jpg" class="card-img grayscale" alt="...">
+            <div class="card-img-overlay" >
+                
+                <div class="container " style="width:65%">
+                    <h1 class="	d-none d-lg-block mt-3 w-50">Cari harga transportasi online yang sesuai</h1>
+                    <div class="row rounded-3 px-4 py-3 mt-5" style="background-color: #E5E4CC; ">
+                        <div class="col-4 d-inline">
+                            <label for="exampleInputEmail1" class="form-label">Pickup Location</label>
+                            <div class=" mx-0 input-group  mb-3">
+                            <input type="email" class="form-control " id="exampleInputEmail1" aria-describedby="emailHelp">
+                                <button class="btn btn-light border" type="button" id="button-addon2" style="background-color: white;"><i class="fas fa-exchange-alt text-secondary"></i></button>
+                            </div>
+                        </div>
+                        <div class="px-0 col-4">
+                            <label for="exampleInputEmail1" class="form-label">Destination Location</label>
+                            <input type="email" class="form-control rounded-5" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        </div>
+                        
+                        <div class="col-2">
+                            <label for="exampleInputEmail1" class="form-label">Type</label>
+                            <select class="form-select" aria-label="Default select example">
+                                
+                                <option value="1">Motorcycle</option>
+                                <option value="2">Car</option>
+                            </select>
+                        </div>
+                        <div class="pt-4 col-1">
+                            <button type="button" class="btn btn-success mt-2">Search</button>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class=" pe-4 m-0 ">
-               <button class="btn btn-default border-start-0 rounded-end " ><i class="fas fa-exchange-alt"></i></button>
-            </div>
-            <div class=" p-0 m-0">
-                <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control large" id="exampleInputPassword1">
-            </div>
-        </div>
-    </form>
-</div>
-
-</div>
-<div>
+      </div>
     
-    <img src="img/home.jpg" class="d-block w-100 img-fluid" alt="">
     
-</div>
-
 @endsection
+
+
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -2,9 +2,7 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
         <a class="navbar-brand" href="/"><h1 class="fw-bold " style="font-family: 'Comfortaa', cursive; color:#00AD42; font-size:3rem">project a1</h1></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+        
         
         <div>
             <button type="button" class="btn btn-outline-success fw-bold px-4 py-2 "><i class="fas fa-globe pe-2"></i>Indonesia</button>
